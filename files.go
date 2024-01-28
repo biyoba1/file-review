ec819ac37756e7e899a229c1daf017dbccd42160
biyoba1 <157849728+biyoba1@users.noreply.github.com>
2024-01-28 16:55:56 +0300

    Add files via upload

---
+++ files.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func calmDown() {
+	recover()
+}
+
+func main() {
+	p := filepath.Join(os.Args[1])
+	files, err := ioutil.ReadDir(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var ways []string
+	for _, file := range files {
+		a := file.Name()
+		path := filepath.Join(p, a)
+		ways = append(ways, path)
+	}
+	for _, folder := range ways {
+		counter(folder)
+	}
+
+}
+func counter(folder string) {
+	files1, err := ioutil.ReadDir(folder)
+	if err != nil {
+		defer calmDown()
+		panic("oh no")
+	}
+	var fileNames []string
+	for _, file := range files1 {
+		if !file.IsDir() {
+			fileNames = append(fileNames, file.Name())
+		}
+	}
+	fmt.Printf("Папка %s содержит файлы: %v\n", folder, fileNames)
+}
