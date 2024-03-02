1df42c30b28e8919c6bc74a0d253b9b3d8822f30
valery <157849728+biyoba1@users.noreply.github.com>
2024-03-02 18:03:03 +0300

    Update files.go

--- files.go
+++ files.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 )
 
 func calmDown() {
@@ -13,28 +15,39 @@ func calmDown() {
 }
 
 func main() {
+	start := time.Now()
 	p := filepath.Join(os.Args[1])
 	files, err := ioutil.ReadDir(p)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	var ways []string
 	for _, file := range files {
 		a := file.Name()
 		path := filepath.Join(p, a)
 		ways = append(ways, path)
 	}
-	for _, folder := range ways {
-		counter(folder)
-	}
 
+	var wg sync.WaitGroup
+	for _, folder := range ways {
+		wg.Add(1)
+		go counter(folder, &wg)
+	}
+	wg.Wait()
+	elapsed := time.Since(start)
+	fmt.Printf("Время выполнения: %s\n", elapsed)
 }
-func counter(folder string) {
+
+func counter(folder string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	files1, err := ioutil.ReadDir(folder)
 	if err != nil {
 		defer calmDown()
-		panic("Cannot read a Dir")
+		panic("oh no")
 	}
+
 	var fileNames []string
 	for _, file := range files1 {
 		if !file.IsDir() {
